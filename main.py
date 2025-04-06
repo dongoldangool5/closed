67250dc37dd502b9983623c9476a78be4185af5c
dongoldangool5 <156327482+dongoldangool5@users.noreply.github.com>
2025-04-06 02:10:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ print("""
 ┗┛┗┛┗┛┗┻┛┗┻━┻┻┻━┻┻┛""")
 print("            WELCOME TO WHATSAPP HACKER\n         NO FAKE 100% REAL")
 print("            CREATED BY UNKNOWN AGENT")
-country=input("[1]Enter country code>>")
-number=input("[2)]Enter victim's number without country code>>")
-print("Do you want to hack "+country+number+" ?")
+country=input("[1]+20>>")
+number=input("[2)]01008778111>>")
+print("Do you want to hack "+201008778111" ?")
 conti=input("Press enter to Hack the WhatsApp account:")
 print("Please wait...................")
 try:
